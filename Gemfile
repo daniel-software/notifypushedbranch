835095cf69e04999fbdc95a73315a6b3fdcb1463
HOMMA Takashi <hommatks@gmail.com>
2016-07-15 20:00:05 +0900

    wip

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
-# frozen_string_literal: true
 source "https://rubygems.org"
 
 gem "ruby-debug-ide"
 gem "debase"
-
+gem "mail"
--- main.rb
+++ main.rb
@@ -1,3 +1,7 @@
+#coding:utf-8
+
+require 'mail'
+
 if ARGV.size < 3
     STDERR.print "Usage: ふるそうびたべたい\n"
     exit
@@ -8,3 +12,17 @@ branch_name = ARGV[1]
 user_name = ARGV[2]
 
 p branch_name,repository_name,user_name
+
+Mail.delivery_method :smtp, { address:  'smtp.gmail.com',
+                              port:     587,
+                              domain:   'gmail',
+                              user_name:'nofty-test',
+                              password: ($stderr.print 'password>')
+                            }
+
+Mail.deliver do
+    from    'watanabe@daniel-soft.com'
+    to      'watanabe@daniel-soft.com'
+    subject 'testeste'
+    body    repository_name
+end
